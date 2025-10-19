ff441706402cddf05f8441868e52ff9f23cc8233
Seyed Mojtaba Mirsoleimani <seyedmojtabamirsoleimani@gmail.com>
2025-10-19 10:00:30 +0330

    Set right authentication_classes in university/views.py.

--- MediCaseBack/university/views.py
+++ MediCaseBack/university/views.py
@@ -2,23 +2,27 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializer import UniversitySerializer, FacultySerializer, DepartmentSerializer
 from .models import University, Faculty, Department
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class UniversityViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = UniversitySerializer
     queryset = University.objects.all()
     lookup_field = 'name'
     lookup_value_regex = '[^/]+'
 
 class FacultyViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = FacultySerializer
     queryset = Faculty.objects.all()
     lookup_field = 'name'
     lookup_value_regex = '[^/]+'
 
 class DepartmentViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = DepartmentSerializer
     queryset = Department.objects.all()
     lookup_field = 'name'
