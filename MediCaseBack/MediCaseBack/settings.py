81c210a5fbc750c721207cf7a70347ac5383b517
Seyed Mojtaba Mirsoleimani <seyedmojtabamirsoleimani@gmail.com>
2025-11-18 17:08:13 +0330

    Fix bugs.

--- MediCaseBack/MediCaseBack/settings.py
+++ MediCaseBack/MediCaseBack/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-CORS_ALLOW_ALL_ORIGINS = Fals
+CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:4200",
