84ff7488df50c23b63e07d6f649e0f8db95e2795
Seyed Mojtaba Mirsoleimani <seyedmojtabamirsoleimani@gmail.com>
2025-12-26 19:14:35 +0330

    Chenge in setting.py.

--- MediCaseBack/MediCaseBack/settings.py
+++ MediCaseBack/MediCaseBack/settings.py
@@ -31,8 +31,6 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:4200",
     "http://localhost:4200",
